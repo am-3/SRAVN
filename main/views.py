7e30e5e23b927591451e88187c1a4055f9c20fde
am-3 <211105@iiitt.ac.in>
2023-10-31 22:37:22 +0530

    setup base routing/urls

--- main/views.py
+++ main/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#Request Handler
 def say_hello(request):
-    return HttpResponse("Hello World")
+    return render(request, 'navbar.html')
