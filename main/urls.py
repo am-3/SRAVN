0f6381910af92ab22d9043d0b5a8d309f7e8a333
am-3 <211105@iiitt.ac.in>
2023-11-01 22:27:10 +0530

    updated routes.urls

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello', views.say_hello),
-    path('new', views.form)
+    path('hello', views.start),
+    path('nav', views.nav),
+    path('form', views.form)
 ]
