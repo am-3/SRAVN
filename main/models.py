ec12f9f3f07c5bebc6fa672aba503a76bd11a7c5
am-3 <211105@iiitt.ac.in>
2023-11-02 21:59:57 +0530

    added models, migrated

--- main/models.py
+++ main/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class eventID(models.Model):
+    event_name = models.CharField(max_length=40)
+    event_id = models.CharField(max_length=5)
+    purpose = models.TextField()
+
+class venue_details(models.Model):
+    venue_room = models.IntegerField()
+    venue_details = models.TextField()
+
+class event_details(models.Model):
+    event_id = models.CharField(max_length=5)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+
+class requirements(models.Model):
+    event_id = models.CharField(max_length=5)
+    proj = models.BooleanField()
+    pa = models.BooleanField()
+
+class event_coordinators(models.Model):
+    event_id = models.CharField(max_length=5)
+    coord_email = models.EmailField()
+
+class event_coordinators_details(models.Model):
+    event_id = models.CharField(max_length=5)
+    coord_name = models.CharField(max_length=40)
+    coord_dept = models.TextChoices('CSE', 'ECE')
+
+class event_sanction(models.Model):
+    event_id = models.CharField(max_length=5)
+    sanction_letter = models.FileField()
+
+class event_status(models.Model):
+    event_id = models.CharField(max_length=5)
+    approved = models.BooleanField()
+    finished = models.BooleanField()
+
+class event_venue(models.Model):
+    event_id = models.CharField(max_length=5)
+    venue_room = models.IntegerField()
--- main/views.py
+++ main/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 #Request Handler
 def start(request):
-    return render(request, 'base.html')
+    template = loader.get_template('base.html')
+    context = {
+        'user' : {
+            'username' : 'hey there lol'
+        }
+    }
+    return HttpResponse(template.render(context, request))
 
 def hello(request):
     return render(request, 'hello.html')
